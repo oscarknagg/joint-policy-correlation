1941503c7bad0b2b27b45232a20fd71cde3ab612
Oscar Knagg <oscar@knagg.co.uk>
2019-08-03 13:57:40 +0100

    Added docstrings and dirty hack for random mazes for envs with even side lengths

--- multigrid/envs/laser_tag/map_generators.py
+++ multigrid/envs/laser_tag/map_generators.py
@@ -103,12 +103,29 @@ class MapPool(LaserTagMapGenerator):
 
 
 def generate_random_mazes(n: int, height: int, width: int, complexity: float, density: float, device: str):
-    """Generates n random mazes that are guaranteed to have good pathing."""
-    if not ((height % 2) == 1 and (width % 2) == 1):
-        raise ValueError('Only odd sizes')
+    """Generates n random mazes where each empty point is guaranteed to be reachable from every other.
 
+    This code is shamelessly ripped from the python code example from here:
+    https://en.wikipedia.org/wiki/Maze_generation_algorithm
+    which is a variant of Prim's algorithm. My only modification is to vectorise this across
+    a batch dimension to generate many mazes in parallel.
+
+    This functions contains a dirty hack to make sure mazes with even side
+    lengths still have good pathing.
+
+    Args:
+        n: Number of mazes to generate
+        height: Height of maze
+        width: Width of maze
+        complexity: Increases the sizes of wall-pieces
+        density: Increases the number of wall-pieces
+        device: Device on which the maze resides e.g. 'cuda', 'cpu'
+    """
     complexity = int(complexity * (5 * (height + width)))  # number of components
     density = int(density * ((height // 2) * (width // 2)))  # size of components
+
+    even_height = int((height % 2) == 0)
+    even_width = int((width % 2) == 0)
 
     pathing = torch.zeros((n, 1, height, width), dtype=torch.uint8, device=device)
     pathing[:, :, 0, :] = 1
@@ -117,10 +134,10 @@ def generate_random_mazes(n: int, height: int, width: int, complexity: float, de
     pathing[:, :, :, -1] = 1
 
     for i in range(density):
-        x = torch.randint(0, width // 2, size=(n,)) * 2
-        y = torch.randint(0, height // 2, size=(n,)) * 2
+        x = (torch.randint(0, (width - 3*even_width) // 2, size=(n,))) * 2
+        y = (torch.randint(0, (height - 3*even_height) // 2, size=(n,))) * 2
 
-        pathing[torch.arange(n), :, x, y] = 1
+        pathing[torch.arange(n), :, y, x] = 1
 
         for j in range(complexity):
             neighbours = torch.zeros((n, 4, 2))
@@ -131,8 +148,8 @@ def generate_random_mazes(n: int, height: int, width: int, complexity: float, de
 
             # Ignores neighbours off the edge
             clamped_neighbours = torch.stack([
-                neighbours[:, :, 0].clamp(0, width),
-                neighbours[:, :, 1].clamp(0, height),
+                neighbours[:, :, 0].clamp(0, width - 1 - 2*even_width),
+                neighbours[:, :, 1].clamp(0, height - 1 - 2*even_height),
             ], dim=-1)
             valid_neighbours = (neighbours == clamped_neighbours).all(dim=-1)
 
@@ -140,17 +157,16 @@ def generate_random_mazes(n: int, height: int, width: int, complexity: float, de
             tmp = drop_duplicates(torch.nonzero(valid_neighbours), 0)[:, 1]
             x_, y_ = neighbours[torch.arange(n), tmp].long().unbind(dim=1)
 
-            # If new position is empty, fill it and the intermediate
-            # position
-            empty = pathing[torch.arange(n), :, x_, y_] == 0
+            # If new position is empty fill that and the intermediate position
+            empty = pathing[torch.arange(n), :, y_, x_] == 0
             intermediate_x = (x + x_) // 2
             intermediate_y = (y + y_) // 2
 
             tmp_pathing = pathing[empty]
 
             n_empty = empty.sum().item()
-            tmp_pathing[torch.arange(n_empty), intermediate_x[empty.squeeze()], intermediate_y[empty.squeeze()]] = 1
-            tmp_pathing[torch.arange(n_empty), x_[empty.squeeze()], y_[empty.squeeze()]] = 1
+            tmp_pathing[torch.arange(n_empty), intermediate_y[empty.squeeze()], intermediate_x[empty.squeeze()]] = 1
+            tmp_pathing[torch.arange(n_empty), y_[empty.squeeze()], x_[empty.squeeze()]] = 1
 
             pathing[empty] = tmp_pathing
 
@@ -161,7 +177,14 @@ def generate_random_mazes(n: int, height: int, width: int, complexity: float, de
 
 
 def generate_random_respawns(pathing: torch.Tensor, n: int, minimum_separation: int = 0):
-    """Generates `n` respawn locations for each pathing map."""
+    """Generates `n` respawn locations for each pathing map given as input.
+
+    Args:
+        pathing: Input pathing maps of shape (num_envs, 1, height, width)
+        n: Number of respawn locations per pathing map
+        minimum_separation: Minimum distance between respawn locations. Setting this too
+            high for a particular map size might result having less than n respawn locations.
+    """
     respawns = torch.zeros_like(pathing)
 
     for i in range(n):
